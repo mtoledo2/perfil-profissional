bf7fe0935406c6734e2d6538da8bd0189bdccffd
mtoledo2 <murilo_toledo@hotmail.com>
2026-02-18 19:47:35 -0300

    v3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,8 @@
 function Footer(){
     return(
-        <div className="border-t border-gray-800 mt-20 text-center w-125 mx-auto py-4">
-            <div className="text-center">
-                <p className="text-sm text-gray-400">Construído com React, Vite e Tailwind CSS</p>
-                <p className="text-sm text-gray-400">© 2026 Murilo Toledo</p>
-            </div>            
+        <div className="border-t border-gray-800 mt-20 text-center max-w-md mx-auto py-4 px-4">
+            <p className="text-sm text-gray-400">Construído com React, Vite e Tailwind CSS</p>
+            <p className="text-sm text-gray-400">© 2026 Murilo Toledo</p>
         </div>
     )
 }
